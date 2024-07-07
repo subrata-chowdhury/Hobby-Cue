f3c2277c9fa2c8e77940b08b5e44850e1d11fc5d
Subrata Chowdhury <subratachowdhury7000@gmail.com>
2024-07-07 15:54:02 +0530

    fixed a bug related to go to top btn

--- src/components/GoToTop.jsx
+++ src/components/GoToTop.jsx
@@ -1,6 +1,6 @@
 function GoToTop() {
     return (
-        <div className="position-fixed bottom-0 end-0 p-4 bg-white rounded-pill d-flex justify-content-center align-items-center m-4" style={{ width: '2rem', height: '2rem', boxShadow: '0 0px 3rem rgba(0, 0, 0, 0.4)', cursor: 'pointer' }} onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
+        <div className="position-fixed bottom-0 end-0 p-4 bg-white rounded-pill d-flex justify-content-center align-items-center m-4" style={{ zIndex: 1000, width: '2rem', height: '2rem', boxShadow: '0 0px 3rem rgba(0, 0, 0, 0.4)', cursor: 'pointer' }} onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
             <img src="expand.svg" alt="Go to top" style={{ rotate: '180deg' }} />
             <a href="#" className="text-muted small" style={{ display: 'none' }}>Go to top (Ctrl+Home)</a>
         </div>
